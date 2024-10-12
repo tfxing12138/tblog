e62b35ec405f4bba3d2a88a0435cce93f3be70b3
link <t18370240624@163.com>
2024-10-12 23:17:42 +0800

    更新数据库连接地址

---
+++ src/main/java/com/tfxing/tblog/entity/constant/DeletedConstant.java
@@ -0,0 +1,8 @@
+package com.tfxing.tblog.entity.constant;
+
+public interface DeletedConstant {
+
+    int DELETED = 1;
+
+    int NOT_DELETE = 0;
+}
--- src/main/java/com/tfxing/tblog/interceptor/TokenInterceptor.java
+++ src/main/java/com/tfxing/tblog/interceptor/TokenInterceptor.java
@@ -2,14 +2,19 @@ package com.tfxing.tblog.interceptor;
 
 import com.alibaba.fastjson.JSONObject;
 import com.tfxing.tblog.utils.TokenUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Component
+@Slf4j
 public class TokenInterceptor implements HandlerInterceptor {
+
+    // 在请求处理之前执行
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
  
@@ -20,10 +25,10 @@ public class TokenInterceptor implements HandlerInterceptor {
         }
         response.setCharacterEncoding("utf-8");
         String token = request.getHeader("token");
-        if (token!=null){
+        if (token != null){
             boolean result= TokenUtils.verify(token);
             if (result){
-                System.out.println("通过拦截器");
+                log.info("通过拦截器{}",token);
                 return true;
             }
         }
@@ -33,7 +38,7 @@ public class TokenInterceptor implements HandlerInterceptor {
             json.put("msg","token verify fail");
             json.put("code","500");
             response.getWriter().append(json.toString());
-            System.out.println("认证失败，未通过拦截器");
+            log.error("认证失败，未通过拦截器");
         } catch (Exception e) {
             return false;
         }
@@ -41,5 +46,19 @@ public class TokenInterceptor implements HandlerInterceptor {
          * 还可以在此处检验用户存不存在等操作
          */
         return false;
+    }
+
+    // 在请求处理之后执行 是在控制器方法执行后、视图渲染之前调用的，如果处理过程中发生异常，它可能不会执行。
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+        log.info("postHandle");
+    }
+
+    // 在请求完全处理后执行 是在整个请求结束后执行的，不管请求是否成功都会调用，主要用于清理资源、记录日志等操作。
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+        log.info("afterCompletion");
     }
 }
